488d209c120899be0fc0a6b22f88fbad33ce4b33
David John Gagne <davidjohngagne@gmail.com>
2024-04-13 14:32:43 -0600

    Hmm

--- scripts/numpy_mem.py
+++ scripts/numpy_mem.py
@@ -5,16 +5,14 @@ import matplotlib.pyplot as plt
 import psutil
 import xarray as xr
 mem = []
-mem.append(psutil.virtual_memory()[3])
 def get_data():
     return np.zeros((1000, 50, 50), dtype=np.float32)
 data = get_data()
-mem.append(psutil.virtual_memory()[3])
 for i in range(data.shape[0]):
     data[i] = np.random.random((50, 50))
-    mem.append(psutil.virtual_memory()[3])
-mem.append(psutil.virtual_memory()[3])
+    mem.append(psutil.virtual_memory()[1])
+mem.append(psutil.virtual_memory()[1])
 xd = xr.DataArray(data)
-mem.append(psutil.virtual_memory()[3])
+mem.append(psutil.virtual_memory()[1])
 plt.plot(mem)
 plt.savefig("mem_profile.png", dpi=200, bbox_inches="tight")
