dc148cc8ce817bd9fcbe0d46909031c1efc8d3d5
Kevin Yang <kevinyang@ucar.edu>
2025-11-21 17:07:18 -0700

    Changed back to the version in main branch
    Moving the tests for the distributed_tensor module to a separate script.

--- bridgescaler/tests/distributed_test.py
+++ bridgescaler/tests/distributed_test.py
@@ -1,11 +1,8 @@
 from bridgescaler.distributed import DStandardScaler, DMinMaxScaler, DQuantileScaler
 from bridgescaler import save_scaler, load_scaler, print_scaler, read_scaler
-from bridgescaler.distributed_tensor import DStandardScalerTensor, DMinMaxScalerTensor
-
 import numpy as np
 import pandas as pd
 import xarray as xr
-import torch
 import os
 from multiprocessing import Pool
 
@@ -192,64 +189,3 @@ if __name__ == "__main__":
     scaled_data_quantile_first = xr_dss_f.transform(test_data_c_first, n_jobs=16)
     stop = perf_counter()
     print(stop - start)
-
-
-def test_dstandard_scaler_tensor():
-    numpy_2d_1 = torch.from_numpy(test_data["numpy_2d"][0])
-    numpy_2d_2 = torch.from_numpy(test_data["numpy_2d"][1])
-    numpy_2d_3 = torch.from_numpy(test_data["numpy_2d"][2])
-    all_ds_2d = torch.vstack([numpy_2d_1, numpy_2d_2, numpy_2d_3])
-    numpy_4d_1 = torch.from_numpy(test_data["numpy_4d"][0])
-    numpy_4d_2 = torch.from_numpy(test_data["numpy_4d"][1])
-    numpy_4d_3 = torch.from_numpy(test_data["numpy_4d"][2])
-    all_ds_4d = torch.vstack([numpy_4d_1, numpy_4d_2, numpy_4d_3])
-    dsses_2d = []
-    dsses_4d = []
-    for n in range(test_data["n_examples"].size):
-        dsses_2d.append(DStandardScalerTensor())
-        dsses_2d[-1].fit(torch.from_numpy(test_data["numpy_2d"][n]))
-        dsses_4d.append(DStandardScalerTensor(channels_last=True))
-        dsses_4d[-1].fit(torch.from_numpy(test_data["numpy_4d"][n]))
-        # save_scaler(dsses_2d[-1], "scaler.json")
-        # new_scaler = load_scaler("scaler.json")
-        # os.remove("scaler.json")
-    dss_total_2d = dsses_2d[0] + dsses_2d[1] + dsses_2d[2]
-    dss_total_4d = dsses_4d[0] + dsses_4d[1] + dsses_4d[2]
-    mean_2d, var_2d = dss_total_2d.get_scales()
-    mean_4d, var_4d = dss_total_4d.get_scales()
-    all_2d_var = all_ds_2d.var(axis=0, unbiased=False)
-    all_4d_var = torch.tensor([all_ds_4d[..., i].var(unbiased=False) for i in range(all_ds_4d.shape[-1])])
-    all_4d_mean = torch.tensor([all_ds_4d[..., i].mean() for i in range(all_ds_4d.shape[-1])])
-    assert mean_2d.shape[0] == test_data["means"].shape[0] and var_2d.shape[0] == test_data["sds"].shape[0], "Stat shape mismatch"
-    assert mean_4d.shape[0] == test_data["means"].shape[0] and var_4d.shape[0] == test_data["sds"].shape[0], "Stat shape mismatch"
-    assert torch.max(torch.abs(mean_2d - all_ds_2d.mean(axis=0))) < 1e-5, "significant difference in means"
-    assert torch.max(torch.abs(var_2d - all_2d_var) / all_2d_var) < 1e-5, "significant difference in variances"
-    assert torch.max(torch.abs(mean_4d - all_4d_mean) / all_4d_mean) < 1e-5, "significant difference in means"
-    assert torch.max(torch.abs(var_4d - all_4d_var) / all_4d_var) < 1e-5, "significant difference in variances"
-
-
-def test_dminmax_scaler():
-    numpy_2d_1 = torch.from_numpy(test_data["numpy_2d"][0])
-    numpy_2d_2 = torch.from_numpy(test_data["numpy_2d"][1])
-    numpy_2d_3 = torch.from_numpy(test_data["numpy_2d"][2])
-    all_ds_2d = torch.vstack([numpy_2d_1, numpy_2d_2, numpy_2d_3])
-    numpy_4d_1 = torch.from_numpy(test_data["numpy_4d"][0])
-    numpy_4d_2 = torch.from_numpy(test_data["numpy_4d"][1])
-    numpy_4d_3 = torch.from_numpy(test_data["numpy_4d"][2])
-    all_ds_4d = torch.vstack([numpy_4d_1, numpy_4d_2, numpy_4d_3])
-    dsses_2d = []
-    dsses_4d = []
-    for n in range(test_data["n_examples"].size):
-        dsses_2d.append(DMinMaxScalerTensor())
-        dsses_2d[-1].fit(torch.from_numpy(test_data["numpy_2d"][n]))
-        dsses_4d.append(DMinMaxScalerTensor())
-        dsses_4d[-1].fit(torch.from_numpy(test_data["numpy_4d"][n]))
-        #save_scaler(dsses_2d[-1], "scaler.json")
-        #new_scaler = load_scaler("scaler.json")
-        #os.remove("scaler.json")
-    dss_total_2d = dsses_2d[0] + dsses_2d[1] + dsses_2d[2]
-    dss_total_4d = dsses_4d[0] + dsses_4d[1] + dsses_4d[2]
-    min_2d, max_2d = dss_total_2d.get_scales()
-    min_4d, max_4d = dss_total_4d.get_scales()
-    assert torch.max(torch.abs(min_2d - all_ds_2d.min(axis=0).values)) < 1e-8, "significant difference in minimum"
-    assert torch.max(torch.abs(max_2d - all_ds_2d.max(axis=0).values)) < 1e-8, "significant difference in maximum"
